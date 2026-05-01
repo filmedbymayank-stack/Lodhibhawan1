ce18578fafdf048a9f2d179becc908e164155eec
filmedbymayank-stack <filmedbymayank@gmail.com>
2026-05-01 15:08:05 +0530

    Add files via upload

--- src/pages/types/index.ts
+++ src/pages/types/index.ts
@@ -1,1 +1,12 @@
-
+export interface ReservationData {
+  id: string;
+  name: string;
+  phone: string;
+  guests: string;
+  datetime: string;
+  status: 'Pending' | 'Confirmed' | 'Cancelled' | 'Reached' | 'DidntReach' | 'Completed' | 'Ongoing' | 'SuggestedNewTime';
+  tableNo?: string;
+  suggestedDatetime?: string;
+  createdAt: string;
+  paymentDoneAt?: string;
+}
