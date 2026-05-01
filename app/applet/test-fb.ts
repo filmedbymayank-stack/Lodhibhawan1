af2bcf35145f236d9d2f9e442b7de64a1ecff6e0
filmedbymayank-stack <filmedbymayank@gmail.com>
2026-05-01 15:03:37 +0530

    Add files via upload

--- app/applet/test-fb.ts
+++ app/applet/test-fb.ts
@@ -1,1 +1,19 @@
+import { getFirestore, doc, getDocFromServer } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import * as fs from 'fs';
 
+const firebaseConfig = JSON.parse(fs.readFileSync('./firebase-applet-config.json', 'utf8'));
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+
+async function run() {
+  try {
+    console.log("Testing connection...");
+    await getDocFromServer(doc(db, 'test', 'connection'));
+    console.log("Success");
+  } catch (err) {
+    console.error("Error connecting:", err);
+  }
+  process.exit(0);
+}
+run();
